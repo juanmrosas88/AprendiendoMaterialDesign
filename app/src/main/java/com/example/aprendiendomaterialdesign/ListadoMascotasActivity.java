40502f2835ed9817d837845ec8f8d9393b494ec6
juanm <juanmrosas8@gmail.com>
2020-09-29 20:12:47 -0300

    En este segundo branch Se crearon dos activitys y sus .java

---
+++ app/src/main/java/com/example/aprendiendomaterialdesign/ListadoMascotasActivity.java
@@ -0,0 +1,14 @@
+package com.example.aprendiendomaterialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListadoMascotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_mascotas);
+    }
+}
